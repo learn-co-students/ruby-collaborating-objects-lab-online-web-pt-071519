b5fa671529cb902518585a66e648ec57072cf1a9
assane savadogo <assane.savadogo81@gmail.com>
2019-08-08 14:46:38 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,7 +33,7 @@ class Artist
  # end
   
   def self.find_or_create_by_name(name)
-    artist = @@all.detect{|artist| artist.name == name}
+    artist = @@all.find{|artist| artist.name == name}
     if artist
       artist
     else 
