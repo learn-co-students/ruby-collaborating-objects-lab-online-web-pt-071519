2be43e0b501e367df8fdd314e030daf495cfe426
janewysoki <jane.wysoki@gmail.com>
2019-09-07 19:05:18 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,22 +19,25 @@ class Artist
   end
   
   def add_song(song)
-    song.artist = self #self here refers to artist we're calling the method on; it's the receiver of the call to this method; particular instance that we're calling add_song on
+    song.artist = self #assigning the song being passed in's artist equal to self
+    #self here refers to the particular instance that we're calling add_song on; artist we're calling the method on; it's the receiver of the call to this method; 
   end
 
   def self.find_or_create_by_name(name)
-    if artist = self.find(name) #self here refers to the Artist class
+    if artist = self.find(name) #self here refers to the Artist class cause this is a class method
       artist
     else
       self.new(name)
     end
   end
   
-  def self.find(name)
+  def self.find(name) #find returns the first object
     @@all.find {|artist| artist.name == name}
   end
 
   def print_songs
+    self.songs.each do |song|
+      puts song.name
+    end
   end
-
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song
   
-  attr_accessor :name, :artist #this is saying that artist belongs to a song
+  attr_accessor :name, :artist #this is saying that an artist belongs to a song
   @@all = []
   
   def initialize(name)
@@ -10,6 +10,19 @@ class Song
   
   def self.all
     @@all
+  end
+
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+    self.artist.add_song(self)
   end
   
 end
